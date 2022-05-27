64c2f8809215cc31a1990b3a05b6ba388b141d99
KaioQueiroga <kaioqcm@alu.ufc.br>
2022-05-27 15:26:33 -0300

    Login (senha retirada)

--- src/controller/login.controller.js
+++ src/controller/login.controller.js
@@ -1,18 +1,13 @@
 class LoginController {
 
   static async create(req, res) {
-    if(req.body.email == undefined || req.body.password == undefined){
-        return res.status(400).json({message:"Data is missing!"})
-    }
- 
-    const {email, password} = req.body
+    const {email} = req.body
     if(email.split("@")[1] == "alu.ufc.br"){
         return res.status(200).json({message:"This email is valid", email})
      }
      return res.status(200).json({message:"This email is not valid", email})
  
      // autenticação com o google ==> Firebase
-     
   }
 } 
 
