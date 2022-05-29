5b837a25f163649445a935205fb9f9da16d7c7ba
DaviChavesPinheiro <davichavespinheiro011010@gmail.com>
2022-05-29 15:09:26 -0300

    Params, Query and Body

--- src/controller/book.controller.js
+++ src/controller/book.controller.js
@@ -1,26 +1,35 @@
-
 class BookController {
 
   static async getAll(req, res) {
-    // TODO: Filtrar por nome, categoria e autor.
-    return res.status(200).json({data: ["book1", "book2", "book3"]}) 
+    const {title, author, categories} = req.query
+
+    return res.status(200).json({data: req.query})
   }
 
   static async getById(req, res) {
-    return res.status(200).json({data: ["book1"]})
+    const {id} = req.params
+
+    return res.status(200).json({data: req.params})
   }
 
   static async create(req, res) {
-    return res.status(200).json({data: ["book1", "book2", "book3"]}) 
+    const {title, author, categories} = req.body
+
+    return res.status(200).json({data: req.body})
   }
 
   static async update(req, res) {
-    return res.status(200).json({data: ["book1", "book2", "book3"]}) 
+    const {id} = req.params
+    const {title, author, categories} = req.body
+
+    return res.status(200).json({data: req.body, id})
   }
 
   static async delete(req, res) {
-    return res.status(200).json({data: ["book1", "book2", "book3"]}) 
+    const {id} = req.params
+
+    return res.status(200).json({data: id})
   }
-} 
+}
 
 module.exports = BookController
--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,20 +1,28 @@
 class UserController {
 
   static async getAll(req, res) {
-    return res.status(200).json({data: ["user1", "user2", "user3"]}) 
+
+    return res.status(200).json({data: ["user1", "user2", "user3"]})
   }
 
   static async getById(req, res) {
-    return res.status(200).json({data: ["user1"]}) 
+  const {id} = req.params
+
+    return res.status(200).json({data: id})
   }
 
   static async update(req, res) {
-    return res.status(200).json({data: ["user1"]}) 
+    const {id} = req.params
+    const {name, email} = req.body
+
+    return res.status(200).json({data: req.body, id})
   }
 
   static async delete(req, res) {
-    return res.status(200).json({data: ["user1"]}) 
+    const {id} = req.params
+
+    return res.status(200).json({data: id})
   }
-} 
+}
 
 module.exports = UserController
