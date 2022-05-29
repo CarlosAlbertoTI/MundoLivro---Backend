dea64c31a13778a7cd326744131a27da5fd4ff26
DaviChavesPinheiro <davichavespinheiro011010@gmail.com>
2022-05-29 15:17:17 -0300

    Book-GetAll route validation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
+const { celebrate, Joi, Segments } = require('celebrate');
 loginController = require("../controller/login.controller.js")
 userController = require("../controller/user.controller.js")
 bookController = require("../controller/book.controller.js")
@@ -11,7 +12,13 @@ router.put('/user/:id', userController.update)
 router.delete('/user/:id', userController.delete)
 
 // Book
-router.get('/book', bookController.getAll)
+router.get('/book', celebrate({
+  [Segments.QUERY]: {
+    title: Joi.string().required(),
+    author: Joi.string().required(),
+    categories: Joi.array().items(Joi.string()).required()
+  }
+}), bookController.getAll)
 router.get('/book/:id', bookController.getById)
 router.post('/book', bookController.create)
 router.put('/book/:id', bookController.update)
